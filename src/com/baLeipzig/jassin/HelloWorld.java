c147d4ddb6c232665c56b5a78a4331c19b0c6dc7
s5002669 <s5002669@ba-sachsen.de>
2024-10-17 08:22:16 +0200

    some more code

--- src/com/baLeipzig/jassin/HelloWorld.java
+++ src/com/baLeipzig/jassin/HelloWorld.java
@@ -8,5 +8,7 @@ public class HelloWorld {
         for (int i = 0; i < args.length; i++) {
             System.out.println(args[i]);
         }
+        System.out.println(1 + 2 + "Studierende");
+        System.out.println("Studierende"+ 1 + 2);
     }
 }
