52dca43a4e980dccc157039217ae3753bce21f01
s5002669 <s5002669@ba-sachsen.de>
2024-10-17 08:19:19 +0200

    rename branch

--- src/com/baLeipzig/jassin/HelloWorld.java
+++ src/com/baLeipzig/jassin/HelloWorld.java
@@ -3,5 +3,10 @@ package com.baLeipzig.jassin;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        System.out.println("Anzahl der Argumente: " + args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
     }
 }
